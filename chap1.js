9d8bfc390b10a5941e73e350ac0fb0a0701cb066
shiekhamberjawed <shiekhamberjawed@gmail.com>
2026-01-10 19:20:42 +0500

    Add files via upload

---
+++ chap1.js
@@ -0,0 +1,29 @@
+// 1. Write a script to greet your website visitor using JS alert 
+// box. 
+alert("Welcome To My portal!");
+//  2. Write a script to display following message on your web 
+// page:
+
+  alert("Error! Please enter a valid password.");
+//   <!-- 3. Script to display the following message using line breaks 
+ alert("Javascript is a advance dynamic...\nlanguage and Interpreting!")
+//  4. Write a script to display following messages in sequence:
+    alert("Welcome to JS Land...");
+
+    alert( "Happy Coding! \n Prevent this page from creating additional dialogs.");
+//     5. Generate the following message through browser’s 
+// developer console:  
+    console.log("Hello... I can run JS through my web browser's console");  
+//     6. Make use of alerts in your new/existing HTML & CSS 
+// project. 
+
+alert("you have successfully logged in");
+// 7. Practice placement of <script></script> element in 
+// following sections of your project in exercise 6: 
+// a. Head 
+// b. Body (before your page’s HTML) 
+// Page 2 of 3 
+// ALERTS | JAVASCRIPT 
+// c. Body (inside your page’s HTML) 
+// d. Body (after your page’s HTML)
+

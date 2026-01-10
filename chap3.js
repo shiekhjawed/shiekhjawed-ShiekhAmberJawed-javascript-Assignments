08b65d18a19cef31ef9f59c0363275ca14c88362
shiekhamberjawed <shiekhamberjawed@gmail.com>
2026-01-10 19:24:42 +0500

    Add files via upload

---
+++ chap3.js
@@ -0,0 +1,35 @@
+// 1. Declare a variable called age & assign to it your age. Show 
+// your age in an alert box.
+var age = 15; // Declare variable age
+alert("I am 15 years old");
+// 2. Declare & initialize a variable to keep track of how many 
+// times a visitor has visited a web page. Show his/her 
+// number of visits on your web page. For example: “You 
+// have visited this site N times”.
+var visitCount = 14; // Initialize visit count
+alert("You have visited this site " + visitCount + " times");
+// 3. Declare a variable called birthYear & assign to it your 
+// birth year. Show the following message in your browser: 
+var birthYear = 2005; // Declare birth year
+
+
+// Show the message in the browser
+document.write("My birth year is " + birthYear + "<br>");
+document.write("Data type of my declared variable is " + typeof(birthYear));
+// 4. A visitor visits an online clothing store 
+// www.xyzClothing.com . Write a script to store in variables 
+// the following information: 
+// b. Product title 
+// c. Quantity i.e. how many products a visitor wants to 
+// order 
+// Show the following message in your browser: “John 
+// Doe ordered 5 T-shirt(s) on XYZ Clothing store”.
+var visitorName = "John Doe"; // Visitor's name
+var productTitle = "T-shirt(s)"; // Product title
+var quantity = 5; // Quantity ordered   
+// Show the order message in the browser
+document.write(visitorName + " ordered " + quantity + " " + productTitle + " on XYZ Clothing store.");
+
+
+
+

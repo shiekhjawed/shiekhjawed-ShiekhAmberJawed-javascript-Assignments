0ff77d52f093abd4fed3578447d91084b967a2f1
shiekhamberjawed <shiekhamberjawed@gmail.com>
2026-01-10 19:21:11 +0500

    Add files via upload

---
+++ chap2.js
@@ -0,0 +1,54 @@
+// 1. Declare a variable called username. 
+var username; // Declare variable
+console.log(username);// Check initial value (undefined)
+// 2. Declare a variable called myName & assign to it a string 
+// that represents your Full Name. 
+var myName = "shiekh Amber Jawed";
+console.log(myName);
+// 3. Write script to 
+// a) Declare a JS variable, titled message. 
+// b) Assign “Hello World” to variable message 
+// c) Display the message in alert box. 
+var message;              // a) Declare variable
+console.log(message);      // Check initial value (undefined)
+message = "Hello World";  // b) Assign value
+alert(message);           // c) Display in alert box
+// 4. Write a script to save student’s bio data in JS variables and 
+// show the data in alert boxes. 
+ // Student bio data
+    var Name = "Shiekh Amber Jawed";
+    var age = 20;
+    var course = "Certified Mobile Application Development";
+
+    // Display data in alert boxes
+    alert(Name);
+    alert(age);
+    alert(course);
+//     5. Write a script to display the following alert using one JS 
+// variable:
+var pizza = "PIZZA\nPIZZ\nPIZ\nPI\nP"; 
+alert(pizza);
+// 6. Declare a variable called email and assign to it a string that 
+// represents your Email Address(e.g. example@example.com). 
+// Show the blow mentioned message in an alert box.(Hint: use 
+// string concatenation) 
+  var email = "@gmail.com";
+  alert("shiekhamberjawed" + email);   
+//   7. Declare a variable called book & give it the value “A 
+// smarter way to learn JavaScript”. Display the following 
+// message in an alert box:  
+var book = "A smarter way to learn from the boook A Smarter\nway to learn JavaScript";
+alert(book);
+// 8. Write a script to display this in browser through JS
+var p = document.createElement("p");//second method to write in browser
+p.innerHTML = "Yah! I can write HTML Through JavaScript";
+document.body.appendChild(p);
+// 9. Store following string in a variable and show in alert and 
+// browser through JS 
+// “▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬” 
+
+// Store the string in a variable
+var specialString = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+
+// Display in an alert box
+alert(specialString);

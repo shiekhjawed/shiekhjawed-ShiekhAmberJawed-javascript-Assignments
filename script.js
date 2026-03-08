68bfbddfff800e21911da784fe8dcb926c265b09
shiekhamberjawed <shiekhamberjawed@gmail.com>
2026-03-08 11:33:23 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+
+
+
+// for (var i = 0; i < 10; i++) {
+//     console.log(i)
+// }
+// var i = 0;
+// while (i < 10) {
+//     console.log(i)
+//     i++
+// }
+
+// var i =0;
+// do {
+//     console.log(i)
+//     i++;
+// }while(i < 0)
+
+// function data(event) {
+//         event.preventDefault(); 
+//     var user = document.getElementById("inputEmail4").value
+//     var password = document.getElementById("inputPassword4").value
+//     console.log(user, password)
+// }
+var name = "Shiekh Amber jawed";
+alert(name);
+var age = 20;
+alert(age)
+var className = 14;
+alert( className)
+
+var education = "graduate";
+alert(education)
+var city = "Karachi";
+alert(city);
+
+
+
+
+
+
+

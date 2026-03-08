c3fdb67c338d572f43fa4f1e926fb270d8c90f9b
shiekhamberjawed <shiekhamberjawed@gmail.com>
2026-03-08 11:34:01 +0500

    var script

--- script.js
+++ script.js
@@ -1,27 +1,6 @@
 
 
 
-// for (var i = 0; i < 10; i++) {
-//     console.log(i)
-// }
-// var i = 0;
-// while (i < 10) {
-//     console.log(i)
-//     i++
-// }
-
-// var i =0;
-// do {
-//     console.log(i)
-//     i++;
-// }while(i < 0)
-
-// function data(event) {
-//         event.preventDefault(); 
-//     var user = document.getElementById("inputEmail4").value
-//     var password = document.getElementById("inputPassword4").value
-//     console.log(user, password)
-// }
 var name = "Shiekh Amber jawed";
 alert(name);
 var age = 20;
@@ -33,6 +12,7 @@ var education = "graduate";
 alert(education)
 var city = "Karachi";
 alert(city);
+
 
 
 
